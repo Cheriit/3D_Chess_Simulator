c617019ba3ef24781fd954d60b42986dd5b7acf9
Szczepaniak-M <m.szczepaniak99@gmail.com>
2020-06-23 16:37:20 +0200

    ambient improve

--- res/shaders/f_shader.glsl
+++ res/shaders/f_shader.glsl
@@ -27,20 +27,20 @@ vec4 color(vec4 lightVector){
     vec4 reflectedVector = reflect(-normalizedLightVector, normalizedNormalVector);
 
     //Parametry powierzchni
-    float ambient = texture(textureMap0, iTexCoord0).r;
+    float ambient = texture(textureMap0, iTexCoord0).r + 0.3f;
     vec4 kd = texture(textureMap1, iTexCoord0) * ambient;
     vec4 ks = vec4(1, 1, 1, 1);
 
     //Obliczenie modelu oświetlenia
     float nl = clamp(dot(normalizedNormalVector, normalizedLightVector), 0, 1);
     float rv = pow(clamp(dot(reflectedVector, normalizedViewerVector), 0, 1), 100);
-    return vec4(kd.rgb*nl,0) + vec4(ks.rgb*rv, 0);
+    return vec4(kd.rgb*nl, 0) + vec4(ks.rgb*rv, 0);
 }
 
 void main(void) {
-    pixelColor = vec4(0,0,0,1); // +skyboxColors();
+    pixelColor = vec4(0, 0, 0, 1);// +skyboxColors();
     for (int i = 0; i< 5; i++){
         pixelColor += color(lightVector[i]);
     }
-   // pixelColor = clamp(pixelColor,0,1);
+    // pixelColor = clamp(pixelColor,0,1);
 }
